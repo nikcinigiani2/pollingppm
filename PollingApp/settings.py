781d9c91e68a872ae34d1350abbde9e58eda9afc
nikcinigiani2 <nik.cinigiani@gmail.com>
2024-06-19 09:49:33 +0200

    Last Commit.2

--- PollingApp/settings.py
+++ PollingApp/settings.py
@@ -132,4 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://pollingppm-production.up.railway.app'
+]
 
